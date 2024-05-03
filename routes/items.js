684e699adc5995cd61917de81e683be55c0685d7
MariaRantanen <48141@edu.raseko.fi>
2024-05-03 16:38:09 +0300

    API

--- routes/items.js
+++ routes/items.js
@@ -9,14 +9,30 @@ const {
     updateItem,
 } = require("../controllers/items");
 
-router.get("/", getItem);
-
-router.post("/", addItem);
-
-router.get("/", getItemId);
-
-router.delete("/", deleteItem)
-
-router.put("/", updateItem);
+router.get("/", (req, res) => {
+    console.log('GET request received for all items');
+    getItem(req, res);
+});
+ 
+router.post("/", (req, res) => {
+    console.log('POST request received for adding an item');
+    console.log('Received data:', req.body); // Log received data from Postman
+    addItem(req, res);
+});
+ 
+router.get("/:id", (req, res) => {
+    console.log('GET request received for item with id:', req.params.id);
+    getItemId(req, res);
+});
+ 
+router.delete("/:id", (req, res) => {
+    console.log('DELETE request received for item with id:', req.params.id);
+    deleteItem(req, res);
+});
+ 
+router.put("/:id", (req, res) => {
+    console.log('PUT request received for item with id:', req.params.id);
+    updateItem(req, res);
+});
 
 module.exports = router;
