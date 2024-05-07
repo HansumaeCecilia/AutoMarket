d7eb91087ae1fdf9d794d63005ac385d05e37bd5
MariaRantanen <48141@edu.raseko.fi>
2024-05-07 11:41:30 +0300

    functions to get and stuf

--- controllers/items.js
+++ controllers/items.js
@@ -1,44 +1,73 @@
 const { v4 } = require("uuid");
+const { pool } = require('../db');
 
-let itemData = [];
 
-const getItem = (req, res) => {
-    res.json(itemData);
+const getItem = async (req, res) => {
+    //const { id } = req.params;
+    try {
+        const result = await pool.query('SELECT * FROM public.cars');
+        res.json(result.rows);
+    } catch (error) {
+        console.error ('Error fetching data', error);
+        res.status(500).send('Internal server error');
+    }
 };
 
-const addItem = (req, res) => {
-    const item = req.body;
-    itemData.push({ ...item, id: v4() });
-    res.send(`Item "${item.name}" has been added successfully.`);
+const addItem = async (req, res) => {
+    const { brand, model, price} = req.body;
+    const itemId = v4();
+    try {
+        const result = await pool.query('INSERT INTO public.cars (brand, model, price) VALUES ($1, $2, $3) RETURNING ', [brand, model, price, itemId]);
+        res.json(result.rows);
+    } catch (error) {
+        console.error ('Error adding item', error);
+        res.status(500).send('Internal server error');
+    }
 };
 
-const getItemId = (req, res) => {
+const getItemId = async (req, res) => {
     const { id } = req.params;
-    const itemFound = itemData.find((item) => item.id === id);
-    console.log(itemFound);
-    res.send(itemFound); 
+    try {
+        const result = await pool.query('SELECT FROM public.cars WHERE id = $1', [id]);
+        if (result.rows.length > 0) {
+            res.json(result.rows[0]); // Return the item found in the database
+        } else {
+            res.status(404).send('Item not found');
+        }
+    } catch (error) {
+        console.error('Error fetching ID:', error);
+        res.status(500).send('Internal server error');
+    }
 };
 
-const deleteItem = (req, res) => {
+const deleteItem = async (req, res) => {
     const { id } = req.params;
-    itemData = itemData.filter((item) => item.id !== id);
-    console.log(itemData);
-    res.send(`Item with id ${id} has been deleted succeffully.`);
-};
+    try {
+        await pool.query('DELETE FROM public.cars WHERE id = $1', [id]);
+        res.send(`Item with ID ${id} has been successfully deleted`)
+    } catch (error) {
+        console.error('Error deleting item:', error);
+        res.status(500).send('Internal server error');
+    }
+  };
 
-const updateItem = (req, res) => {
+const updateItem = async (req, res) => {
     const { id } = req.params;
-    const { name, toppings, price } = req.body;
-
-    const item = itemData.find((item) => item.id === id);
-
-    if (name) item.name = name;
-    if (toppings) item.toppings = toppings;
-    if (price) item.price = price;
+     try {
+        await pool.query('UPDATE public.cars SET brand = $1, model = $2, price = $3, id = $4', [brand, model, price, id]);
+        if (result.rows.length > 0) {
+            res.json(result.rows[0]); // Return the item found in the database
+        } else {
+            res.status(404).send('Item not found');
+        }
+    } catch (error) {
+        console.error('Error updating item:', error);
+        res.status(500).send('Internal server error');
+    }
 
     res.send(
-        `Item with id ${id} and name ${item.name} has been updated successfully`
+        `Item with id ${id} has been updated successfully`
     );
 };
 
-module.exports = {getItem, addItem, getItemId, deleteItem, updateItem};
+module.exports = { getItem, addItem, getItemId, deleteItem, updateItem };
