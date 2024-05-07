6f39cb24c271cda78e548924fdcb8944d4c60eac
MariaRantanen <48141@edu.raseko.fi>
2024-05-07 11:41:44 +0300

    item routes

--- routes/items.js
+++ routes/items.js
@@ -13,23 +13,23 @@ router.get("/", (req, res) => {
     console.log('GET request received for all items');
     getItem(req, res);
 });
- 
+
 router.post("/", (req, res) => {
     console.log('POST request received for adding an item');
     console.log('Received data:', req.body); // Log received data from Postman
     addItem(req, res);
 });
- 
+
 router.get("/:id", (req, res) => {
     console.log('GET request received for item with id:', req.params.id);
     getItemId(req, res);
 });
- 
+
 router.delete("/:id", (req, res) => {
     console.log('DELETE request received for item with id:', req.params.id);
     deleteItem(req, res);
 });
- 
+
 router.put("/:id", (req, res) => {
     console.log('PUT request received for item with id:', req.params.id);
     updateItem(req, res);
