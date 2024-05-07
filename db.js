8e07f59af0104f768817fd482fb118a565fb3c91
MariaRantanen <48141@edu.raseko.fi>
2024-05-07 11:40:05 +0300

    db setting created

---
+++ db.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'AutoMarket',
+    password: 'Q2werty',
+    port: 5432,
+});
+
+module.exports = { pool };
